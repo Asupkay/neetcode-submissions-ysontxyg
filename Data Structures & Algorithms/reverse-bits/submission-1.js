e3934f5f27fb561a32ccec235e62a1d89a5fc301
Alex Supkay <asupkay1124@gmail.com>
2026-05-01 19:11:32 -0400

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n) {
+
+        let newNum = 0;
+        for (let i = 0; i < 32; i++) {
+            const lastBit = n & 1;
+            newNum = (newNum << 1) + lastBit;
+            n = n >> 1;
+        }
+
+        return newNum >>> 0;
+    }
+}
