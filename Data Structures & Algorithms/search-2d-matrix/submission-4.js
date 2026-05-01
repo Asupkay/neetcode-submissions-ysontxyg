882549e57aecdcb3ae224b27b108bffeef1db4b6
Alex Supkay <asupkay1124@gmail.com>
2026-05-01 12:20:51 -0400

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let l = 0;
+        let r = matrix.length * matrix[0].length - 1;
+
+        while(l <= r) {
+            const mid = Math.floor((r - l)/2) + l;
+
+            const row = Math.floor(mid / matrix[0].length)
+            const column = mid % matrix[0].length
+            console.log(mid, row, column)
+
+            if (matrix[row][column] === target) {
+                return true;
+            }
+            if (matrix[row][column] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return false;
+
+    }
+}
