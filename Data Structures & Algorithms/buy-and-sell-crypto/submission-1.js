eaa99234fdf12982762d99da96978c343d2ff7bc
Alex Supkay <asupkay1124@gmail.com>
2026-05-04 12:33:34 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let cLow = prices[0];
+        for (let i = 1; i < prices.length; i++) {
+            const cPrice = prices[i];
+            maxProfit = Math.max(maxProfit, cPrice - cLow);
+            cLow = Math.min(cLow, cPrice);
+        }
+        return maxProfit;
+    }
+}
